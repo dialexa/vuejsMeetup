1e74dd2844aea5ed2c59229a631c4bf37ea40576
Tim Waite <tawaite@gmail.com>
2018-07-18 19:48:48 -0500

    Adding directives demo code

---
+++ Meetup1/directives-demo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="content">
+        <img src="./assets/logo.png" height="30">
+        <h2>Directives</h2>
+      </div>
+    </header>
+    <Todos/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #dfe5ec;
+}
+
+.content {
+  display: flex;
+  width: 1024px;
+  align-items: center;
+  margin: 0 3em;
+}
+
+.content > h2 {
+  margin-left: 0.5em;
+}
+</style>
