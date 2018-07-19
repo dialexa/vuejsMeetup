75a707a45dd62804a2efb7ba8c223287c463d952
Joseph Campuzano <campuzanojoseph@gmail.com>
2018-07-19 10:17:06 -0500

    moving over some more logic. Adding todo-item component

--- Meetup1/integration-demo/src/todo.vue.js
+++ Meetup1/integration-demo/src/todo.vue.js
@@ -1,7 +1,53 @@
 const TodoVue = new Vue({
   el: '#vue-todos',
 
-  mounted() {
-    console.log('vue todos');
+  data: {
+    text: '',
+    todos: todoService.getTodos(),
+  },
+
+  // watch: {
+  //   todos() {
+  //     // todoService.saveTodos();
+  //   },
+  // },
+
+  methods: {
+    submit() {
+      if (!this.text.trim()) return;
+
+      const todo = createTodo(this.text.trim());
+      this.todos.push(todo);
+    },
   },
 });
+
+function createTodo(text, completed = false) {
+  return {
+    text,
+    completed,
+    id: guid(),
+  };
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return (
+    s4() +
+    s4() +
+    '-' +
+    s4() +
+    '-' +
+    s4() +
+    '-' +
+    s4() +
+    '-' +
+    s4() +
+    s4() +
+    s4()
+  );
+}
