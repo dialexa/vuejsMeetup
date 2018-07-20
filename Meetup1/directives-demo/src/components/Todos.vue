f81b807f5791bbc2a5b68f30d60dbfda939a836e
Timothy Waite <twaite@Timothys-MacBook-Pro.local>
2018-07-19 23:02:41 -0500

    Update with moar

--- Meetup1/directives-demo/src/components/Todos.vue
+++ Meetup1/directives-demo/src/components/Todos.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- modifiers with override and prevent -->
-  <div tabindex="0" class="todos" @keydown.ctrl.s.prevent="save">
+  <div tabindex="0" class="todos" @keydown.meta.s.prevent="save">
     <!-- v-if -->
     <h2 v-if="!todos.length">Hello, what would you like to do today?</h2>
     <!-- v-else-if -->
@@ -12,15 +12,22 @@
     <!-- v-on -->
     <button @click="addTodo">Add</button>
     <!-- v-show -->
-    <div class="missing" :class="{'danger': todos.length > 5}">
+    <div class="notes" :class="{'danger': todos.length > 5}">
       <h2 v-show="!todos.length">
         Add a todo!
       </h2>
       <!-- v-for -->
-      <div :class="['todo', {'done': todo.done}]" v-for="todo in todos" :key="todo.id">
-        <input type="checkbox" v-model="todo.done">{{todo.name}}
+      <div :class="['todo', {'done': todo.done}]" v-for="(todo, index) in todos" :key="todo.id" @click="todo.done = !todo.done">
+        <input type="checkbox" v-model="todo.done">
+        <span class="todo__message">{{todo.message}}</span>
+        <button class="delete" @click.stop="remove(index)">X</button>
       </div>
     </div>
+    <transition name="slide-fade">
+      <div class="toast" v-show="showToast">
+        {{toastMessage}}
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -28,21 +35,33 @@
 export default {
   data () {
     return {
-      todos: [],
-      newTodo: ''
+      todos: JSON.parse(window.localStorage.getItem('todos')) || [],
+      newTodo: '',
+      toastMessage: '',
+      showToast: false
     };
   },
   methods: {
     addTodo () {
       this.todos.push({
-        name: this.newTodo,
+        message: this.newTodo,
         done: false,
         id: `todo-${this.todos.length}`
       });
       this.newTodo = '';
     },
     save () {
-      alert('We could go save the list here!');
+      window.localStorage.setItem('todos', JSON.stringify(this.todos));
+      this.toast('Saved notes');
+    },
+    remove (index) {
+      this.todos.splice(index, 1);
+    },
+    toast (message) {
+      this.toastMessage = message;
+      this.showToast = true;
+      let self = this;
+      setTimeout(() => { self.showToast = false; }, 2000);
     }
   }
 };
@@ -62,11 +81,10 @@ input[type="text"] {
   border-bottom: 2px solid lightslategray;
   width: 300px;
   outline: none;
-  padding: .5em;
+  padding: .6em;
 }
 
 input:focus {
-  padding-bottom: .7em;
   border-color: slateblue;
 }
 
@@ -79,7 +97,7 @@ button {
   cursor: pointer;
 }
 
-.missing {
+.notes {
   margin: 2em 1em;
   padding: 3em;
   background: whitesmoke;
@@ -104,6 +122,18 @@ button {
   align-items: center;
   font-size: 1.2em;
   margin: .5em 0;
+  cursor: pointer;
+}
+
+.todo__message {
+  flex-grow: 1;
+  display: flex;
+}
+
+.delete {
+  color: #2c3e50;
+  align-self: right;
+  background: none;
 }
 
 .done {
@@ -112,5 +142,29 @@ button {
 
 input[type="checkbox"] {
   margin-right: .5em;
+}
+
+.toast {
+  position: absolute;
+  background-color: rgba(106, 90, 205, .9);
+  border: 1px solid slateblue;
+  border-radius: .2em;
+  top: 2em;
+  right: 2em;
+  padding: 1em;
+  color: white;
+  font-weight: bold;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
